c2669144d24c66e2d3daa917c92a417bb317634c
ProgramistaiJa <programista@i-ja.pl>
2022-11-26 14:18:58 +0100

    feat: Added prism overriders css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
-// import '../styles/prism-overrides.css';
+import '../styles/prism-overrides.css';
 import 'styles/index.css';
 
 function MyApp({ Component, pageProps }) {
